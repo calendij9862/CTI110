ed7985aca32b2763069f324c921e8b1e280f9773
calendij9862 <71242839+calendij9862@users.noreply.github.com>
2020-09-13 23:59:06 -0400

    Add files via upload
    First submission

---
+++ P1HW2_BasicMath_CalendineJoseph.py
@@ -0,0 +1,22 @@
+# This program adds and multiplies 2 numbers
+# 9/13/2020
+# CTI-110 P1HW2 - Basic Math
+# Joseph Calendine
+#
+
+print('Please enter your first number:')
+Integer1 = int(input())
+print( 'Please enter your second number:')
+Integer2 = int(input())
+print( 'First number entered: ' , Integer1 )
+print( 'Second number entered: ' , Integer2 )
+print()
+print( 'Sum of both numbers: ' , Integer1 + Integer2 )
+print( 'Result of Multiplying the two numbers: ' , Integer1 * Integer2 )
+
+       
+# print prompt to enter string 1 and convert to integer
+# print prompt to enter string 2 and convert to integer
+# print both inputs
+# print sum of both numbers
+# print product of both numbers
